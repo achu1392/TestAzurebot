107e4da096f115240c51b65eaae1f6e2555e614f
Ashwin <ashwinm1392@gmail.com>
2019-03-06 21:59:26 -0800

    new test azure webapp bot -42

--- dialogs/colours/index.js
+++ dialogs/colours/index.js
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const ColourCard = require('./resources/ColourCard.json');
+const ColourCard = require('./resources/colourCard.json');
 
 exports.ColourCard = ColourCard;
