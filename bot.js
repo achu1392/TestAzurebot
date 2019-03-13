e997c1f4afd480a638e12ffd21424a86f8713c14
Ashwin <ashwinm1392@gmail.com>
2019-03-13 10:15:59 -0700

    new test azure webapp bot -91

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ const GREETING_INTENT = 'Greeting';
 const CANCEL_INTENT = 'Cancel';
 const HELP_INTENT = 'Help';
 const NONE_INTENT = 'None';
-const FRUIT_INTENT = 'Yellow';
+const FRUIT_INTENT = 'Orange';
 
 
 // Supported LUIS Entities, defined in ./dialogs/greeting/resources/greeting.lu
