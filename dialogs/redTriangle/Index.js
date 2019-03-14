8757e9f8ddbf111c271005de275ac4837a633acb
Ashwin <ashwinm1392@gmail.com>
2019-03-14 12:55:09 -0700

    new test azure webapp bot -114

---
+++ dialogs/redTriangle/Index.js
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const RedTriangleCard = require('./resources/redTriangleCard.json');
+
+exports.RedTriangleCard = RedTriangleCard;
