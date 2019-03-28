96c34bf87720959f7dcb152b37f637c51b7d191c
Ashwin <ashwinm1392@gmail.com>
2019-03-28 14:24:16 -0700

    new test azure webapp bot -143

--- dialogs/roundYellowShellNoCandle/index.js
+++ dialogs/roundYellowShellNoCandle/index.js
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const RoundYellowShellNoCandle = require('./resources/roundYellowShellNoCandle.json');
+const RoundYellowShellNoCandle = require('./resources/roundYellowShellNoCandleCard.json');
 
 exports.RoundYellowShellNoCandle = RoundYellowShellNoCandle;
